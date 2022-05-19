d2e5a26a74a99846afe5191925cc72bc68647947
Shantanu <Shantanu@ShantanuK.bbdnet.bbd.co.za>
2022-05-19 16:52:29 +0530

    Added templates for mobile store

--- mobile_store/models.py
+++ mobile_store/models.py
@@ -1,5 +1,6 @@
 from django.db import models
 from django.core.validators import MaxValueValidator, MinValueValidator
+from django.urls import reverse
 
 # Create your models here.
 
@@ -10,7 +11,10 @@ class Mobile(models.Model):
     price = models.DecimalField(max_digits=10, decimal_places=2)
     rating = models.DecimalField(null=True, max_digits=4, decimal_places=2, validators=[
                                  MaxValueValidator(10), MinValueValidator(1)])
-    is_recommended = models.BooleanField(default=False) 
+    is_recommended = models.BooleanField(default=False)
+
+    def get_absolute_url(self):
+        return reverse("mobile_details", kwargs={"id": self.id})
 
     def __str__(self) -> str:
         return f"{self.make} {self.name} @ {self.price}."
--- mobile_store/views.py
+++ mobile_store/views.py
@@ -1,3 +1,27 @@
 from django.shortcuts import render
+from django.http import Http404
+
+from .models import Mobile
 
 # Create your views here.
+
+
+def index(request):
+    all_mobiles = Mobile.objects.all().order_by('make')
+    return render(request, "mobile_store/index.html", {
+        "mobiles": all_mobiles
+    })
+
+
+def mobile_details(request, id):
+    try:
+        mobile = Mobile.objects.get(pk=id)
+        return render(request, "mobile_store/mobile_details.html", {
+            "name": mobile.name,
+            "make": mobile.make,
+            "price": mobile.price,
+            "rating": mobile.rating,
+            "is_recommended": mobile.is_recommended
+        })
+    except:
+        raise Http404
